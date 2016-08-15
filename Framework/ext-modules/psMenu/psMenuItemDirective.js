992858c44976eaf4543917ca719520a9804d5b7e
gonghaima@hotmail.com <5418854188>
2016-08-15 22:06:29 +1200

    Building a Menu Component in AngularJS - Specifying Icons

--- Framework/ext-modules/psMenu/psMenuItemDirective.js
+++ Framework/ext-modules/psMenu/psMenuItemDirective.js
@@ -5,7 +5,8 @@ angular.module("psMenu").directive('psMenuItem', function () {
     return {
         require:'^psMenu',
         scope: {
-            label: '@'
+            label: '@',
+            icon:'@'
         },
         templateUrl: "ext-modules/psMenu/psMenuItemTemplate.html",
         link: function (scope, el, attr, ctrl) { }
