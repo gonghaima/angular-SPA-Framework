2cac1106a0d784b18cfad5dbd758d7154acfd7ba
gonghaima@hotmail.com <5418854188>
2016-08-23 23:22:27 +1200

    Building a Menu Component in AngularJS - Styling the Responsive menu

--- Framework/ext-modules/psFramework/psFrameworkController.js
+++ Framework/ext-modules/psFramework/psFrameworkController.js
@@ -1,11 +1,29 @@
 ﻿"use strict";
 
 angular.module("psFramework").controller("psFrameworkController",
-    ['$scope', 
-        function ($scope) {
+    ['$scope', '$window', '$timeout',
+        function ($scope, $window, $timeout) {
+            $scope.isMenuVisible = true;
             $scope.isMenuButtonVisible = true;
             $scope.$on('ps-menu-item-selected-event', function (evt, data) {
                 $scope.routeString = data.route;
             });
+            $($window).on('resize.psFramework', function () {
+                $scope.$apply(function () {
+                    checkWidth();
+                });
+            });
+            $scope.$on('$destroy', function () {
+                $($window).off("resize.psFramework");
+            });
+            var checkWidth = function () {
+                var width = Math.max($($window).width(), $window.innerWidth);
+                $scope.isMenuVisible = (width > 768);
+                $scope.isMenuButtonVisible = !$scope.isMenuVisible;
+            };
+
+            $timeout(function () {
+                checkWidth();
+            }, 0);
         }
     ]);
