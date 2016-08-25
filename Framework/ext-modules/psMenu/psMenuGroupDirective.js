42549d92d964beb2581033630a591bfafeaec148
gonghaima@hotmail.com <5418854188>
2016-08-25 21:41:59 +1200

    Advanced Menu Features - Multiple Menu Levels

---
+++ Framework/ext-modules/psMenu/psMenuGroupDirective.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="F:\Study\spaWithAngular\Steven work files\Framework\Framework\Scripts/angular.min.js" />
+"use strict";
+
+angular.module("psMenu").directive('psMenuGroup', function () {
+    return {
+        require: '^psMenu',
+        transclude: true,
+        scope: {
+            label: '@',
+            icon: '@'
+        },
+        templateUrl: "ext-modules/psMenu/psMenuGroupTemplate.html",
+        link: function (scope, el, attr, ctrl) {
+            scope.isOpen = false;
+            scope.colseMenu = function () {
+                scope.isOpen = false;
+            };
+            scope.clicked = function () {
+                scope.isOpen = !scope.isOpen;
+            };
+        }
+    };
+});
